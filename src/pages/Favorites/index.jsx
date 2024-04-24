119d44ee5215af651c0584cb5824720a90704a4d
KTDKasp <churroskasp@gmail.com>
2024-04-24 22:10:41 +0300

    Верстка для пустой /favorites

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,37 +1,67 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from '../../components/Card';
 
 import './Favorites.css';
+import axios from 'axios';
 
 export const Favorites = () => {
+  const [favoritesItems, setFavoritesItems] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get('https://6d35450ae5876ee3.mokky.dev/favorites')
+      .then((res) => {
+        setFavoritesItems(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="favorites">
-      <div className="favorites__top">
-        <Link to="/">
-          <img
-            className="favorites__top-arrow"
-            src="/svg/arrow-right.svg"
-            alt="Arrow"
-          />
-        </Link>
-        <h2 className="content__h2">Мои закладки</h2>
-      </div>
-      <div className="card-list">
-        <Card
-          isAdded={false}
-          id={3}
-          price={12990}
-          title={'Nike CactusJack Air Jordan 720'}
-          imageUrl={'/sneakers/sneakers-5.jpg'}
-        />
-        <Card
-          isAdded={true}
-          id={4}
-          price={12990}
-          title={'Nike CactusJack Air Jordan 720'}
-          imageUrl={'/sneakers/sneakers-7.jpg'}
-        />
-      </div>
+      {favoritesItems.length > 0 ? (
+        <>
+          <div className="favorites__top">
+            <Link to="/">
+              <img
+                className="favorites__top-arrow"
+                src="/svg/arrow-right.svg"
+                alt="Arrow"
+              />
+            </Link>
+            <h2 className="content__h2">Мои закладки</h2>
+          </div>
+          <div className="card-list">
+            {favoritesItems.map((item) => (
+              <Card
+                key={item.id}
+                isAdded={false}
+                id={item.id}
+                price={item.price}
+                title={item.title}
+                imageUrl={item.imageUrl}
+                onClickAdd={() => console.log(item)}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="favorites__empty">
+          <img src="/png/emoji-1.png" alt="Sad emoji" />
+          <div className="favorites__text-empty">
+            <h2>{'Закладок нет :('}</h2>
+            <p>Вы ничего не добавляли в закладки</p>
+          </div>
+          <Link className='favorites__back' to="/">
+            <button className="button_green drawer__button_green">
+              <img src="/svg/arrow-next.svg" alt="Arrow" />
+              Вернуться назад
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
